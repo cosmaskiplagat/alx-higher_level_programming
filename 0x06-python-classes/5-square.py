04b2e42c68666ba9cc2371ed7b46f4abfc8ea3a8
cosmaskiplagat <kiplagatcosmas@hotmail.com>
2023-12-19 11:40:29 -0800

    last batch

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 
-
 """Define a class Square."""
 
 
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Initialize a new square.
 
         Args:
@@ -31,4 +30,12 @@ class Square:
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
 
